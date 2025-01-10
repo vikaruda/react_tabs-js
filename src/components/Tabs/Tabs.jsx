c12a2f88eac27a8014a00b5132cbba15dbb2e4f8
Vika Ruda <rudaviktoria0@gmail.com>
2025-01-10 21:03:13 +0200

    Solution

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,1 +1,34 @@
-export const Tabs = () => {};
+import classNames from 'classnames';
+
+export const Tabs = ({ tabs, selectedTabId, onTabSelected }) => {
+  return (
+    <div data-cy="TabsComponent">
+      <div className="tabs is-boxed">
+        <ul>
+          {tabs.map(tabe => (
+            <li
+              className={classNames('no-active', {
+                'is-active': tabe.id === selectedTabId,
+              })}
+              data-cy="Tab"
+            >
+              <a
+                href={`#${tabe.id}`}
+                data-cy="TabLink"
+                onClick={e => {
+                  e.preventDefault(); // Prevent default anchor behavior
+                  onTabSelected(tabe); // Pass the selected tab to the callback
+                }}
+              >
+                {tabe.title}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div className="block" data-cy="TabContent">
+        {tabs.find(tab => tab.id === selectedTabId)?.content}
+      </div>
+    </div>
+  );
+};
